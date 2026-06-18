f569092de5c5ad3b437938392b0ee142768790a3
elixiao <elixiao@tencent.com>
2026-06-18 18:46:55 +0800

    feat: upgrade deps and tests
    - loose does not change the for-of case result
    - destructuring case now always pass, so deleted

--- src/destructuring.test.js
+++
@@ -1,8 +0,0 @@
-import test from 'ava'
-
-test('babel destructuring test fails with loose:true', function (t) {
-  t.notThrows(function (x, y, z) {
-    const [a, ...rest] = arguments
-    return [a, rest]
-  }.bind(null, 1, 2, 3))
-})
